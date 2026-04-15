bad4199b90f02b0027f741dd4927d60061394305
Mohamed Tarek <mohdibntarek@gmail.com>
2026-04-15 08:01:26 +0400

    make quarto content more searchable

--- build.sh
+++ build.sh
@@ -15,9 +15,11 @@ cp -r quarto_blog/_site public/quarto_blog
 astro build
 
 # Add data-pagefind-ignore to code blocks and code-tools button
+# Copy data-anchor-id to id on Quarto headings for Pagefind section indexing
 find dist/ -name '*.html' -exec sed -i.bak \
   -e 's/class="sourceCode/data-pagefind-ignore class="sourceCode/g' \
   -e 's/class="btn code-tools-button"/data-pagefind-ignore class="btn code-tools-button"/g' \
+  -e 's/class="anchored" data-anchor-id="\([^"]*\)"/class="anchored" data-anchor-id="\1" id="\1"/g' \
   {} +
 find dist/ -name '*.bak' -delete
 
